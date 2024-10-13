2585c6de6926d76988be47d03e7b63b096eddd72
MYSK <mysk@simcorp.com>
2024-10-13 16:12:39 +0300

    Portfolio Current Value calculation

--- Homework2/CurrentValue.cs
+++ Homework2/CurrentValue.cs
@@ -7,11 +7,30 @@ using System.Threading.Tasks;
 
 namespace Homework2
 {
+    enum TransactionType
+    { Buy = 1, Sell = -1}
     internal class CurrentValue
     {
         static void Main(string[] args)
         {
-            CurrentValue = Sign * Nominal * Price;
+            TransactionType trcType;
+            Console.WriteLine("Input the Transaction Type of the trade:");
+            string userInput1 = Console.ReadLine();
+            trcType = (TransactionType)Enum.Parse(typeof(TransactionType), userInput1, true);
+
+
+            Console.WriteLine("Input the Nominal of the trade:");
+            string userInput2 = Console.ReadLine();
+            int nominal = int.Parse(userInput2);
+
+            Console.WriteLine("Input the Price of the trade:");
+            string userInput3 = Console.ReadLine();
+            double price = Double.Parse(userInput3);
+
+            var CurrentValue = (int)trcType * nominal * price;
+            Console.WriteLine($"Portfolio Current Value is {CurrentValue}");
+
+
         }
     }
 }
