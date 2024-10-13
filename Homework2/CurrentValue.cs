934a21b3fbdc8d9b0d1e86fc5d6b7e37368a258d
MYSK <mysk@simcorp.com>
2024-10-13 11:28:13 +0300

    first commit

---
+++ Homework2/CurrentValue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    internal class CurrentValue
+    {
+        static void Main(string[] args)
+        {
+            CurrentValue = Sign * Nominal * Price;
+        }
+    }
+}
